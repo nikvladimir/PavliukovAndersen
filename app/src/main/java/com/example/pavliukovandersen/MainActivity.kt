2bed97f2b6cbb8fa80ace4030c3ed187d801a726
Vladimir <alfa.voland@mail.ru>
2023-05-11 17:16:25 +0600

    FAT-56530: added first fragment

--- app/src/main/java/com/example/pavliukovandersen/MainActivity.kt
+++ app/src/main/java/com/example/pavliukovandersen/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.pavliukovandersen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pavliukovandersen.FirstFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragmentContainer, FirstFragment())
+        fragmentTransaction.commit()
     }
 }
