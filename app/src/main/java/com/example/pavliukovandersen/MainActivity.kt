34127864c5ce8c679a406c1fc14292689b30975c
Vladimir <alfa.voland@mail.ru>
2023-05-17 14:05:31 +0600

    FAT-56530: created AlerdDialog

--- app/src/main/java/com/example/pavliukovandersen/MainActivity.kt
+++ app/src/main/java/com/example/pavliukovandersen/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.pavliukovandersen
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlin.random.Random
+
+val GEOMETRIC_SHAPES = listOf(
+    "circle", "oval", "triangle", "square", "rectangle",
+    "rhombus", "kite", "hexagon", "octagon", "semicircle"
+)
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var alertButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +25,29 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.fragmentContainerText, TextViewFragment())
         fragmentTransaction.replace(R.id.fragmentContainerEditText, EditTextFragment())
         fragmentTransaction.commit()
+
+        alertButton = findViewById(R.id.alertButton)
+        alertButton.setOnClickListener { showAlertDialog() }
+    }
+
+    private fun showAlertDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        val messageForAlert = getRandomElementWithIndexFromList(GEOMETRIC_SHAPES)
+        alertDialogBuilder.setTitle("Alert")
+        alertDialogBuilder.setMessage(messageForAlert)
+
+        alertDialogBuilder.setPositiveButton("OK") { dialog, which ->
+            // Positive button clicked
+            // Perform any action here
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
+    private fun getRandomElementWithIndexFromList(list: List<String>): String {
+        val randomIndex = Random.nextInt(0, list.size)
+        val randomElement = list[randomIndex]
+        return "$randomIndex ($randomElement)"
     }
 }
