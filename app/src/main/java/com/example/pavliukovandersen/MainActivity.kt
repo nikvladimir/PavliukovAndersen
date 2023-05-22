e4d81a404591d7e502b1a93cf70ae580bec1cb3b
Vladimir <alfa.voland@mail.ru>
2023-05-22 21:47:04 +0600

    FAT-56530:  added drawerLayout for lessons

--- app/src/main/java/com/example/pavliukovandersen/MainActivity.kt
+++ app/src/main/java/com/example/pavliukovandersen/MainActivity.kt
@@ -4,6 +4,11 @@ import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pavliukovandersen.databinding.ActivityMainBinding
@@ -15,6 +20,9 @@ val GEOMETRIC_SHAPES = listOf(
 )
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var conf: AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var alertButton: Button
 
@@ -23,10 +31,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentManager.beginTransaction().replace(R.id.displayed_screen_fl, TextViewFragment())
+            .commit()
+
+        binding.apply {
+
+            navigationMenu.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.Lesson1 -> fragmentManager.beginTransaction().replace(
+                        R.id.displayed_screen_fl, TextViewFragment()
+                    ).commit()
+
+                    R.id.Lesson2 -> fragmentManager.beginTransaction().replace(
+                        R.id.displayed_screen_fl, Lesson2fragment()
+                    ).commit()
+
+                    R.id.Lesson3 -> fragmentManager.beginTransaction().replace(
+                        R.id.displayed_screen_fl, Lesson3fragment()
+                    ).commit()
+                }
+                activityMainDrawer.closeDrawer(GravityCompat.START)
+                true
+            }
+        }
+
 
 //        fragmentTransaction.replace(R.id.fragmentContainerText, TextViewFragment())
 //        fragmentTransaction.replace(R.id.fragmentContainerEditText, EditTextFragment())
