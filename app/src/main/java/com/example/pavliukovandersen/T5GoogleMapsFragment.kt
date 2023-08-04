35e74fe788c5c333153157061db9fed8f25906f7
Vladimir <alfa.voland@mail.ru>
2023-08-04 16:42:25 +0600

    FAT-58350: added location checker

--- app/src/main/java/com/example/pavliukovandersen/T5GoogleMapsFragment.kt
+++ app/src/main/java/com/example/pavliukovandersen/T5GoogleMapsFragment.kt
@@ -1,37 +1,79 @@
 package com.example.pavliukovandersen
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
 class T5GoogleMapsFragment : Fragment() {
 
+    private lateinit var mapView: MapView
+    private val mapReadyCallback = OnMapReadyCallback { googleMap ->
+
+        checkLocationServicesEnabled()
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val rootView = inflater.inflate(R.layout.t5_fragment_google_maps, container, false)
+        mapView = rootView.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(mapReadyCallback)
+        return rootView
+    }
 
-        val view: View = inflater.inflate(R.layout.t5_fragment_google_maps, container, false)
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
 
-        val supportMapFragment =
-            childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment?
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
 
-        supportMapFragment!!.getMapAsync { googleMap ->
-            googleMap.setOnMapClickListener { latLng ->
-                val markerOptions = MarkerOptions()
-                markerOptions.position(latLng)
-                markerOptions.title(latLng.latitude.toString() + " : " + latLng.longitude)
-                googleMap.clear()
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
-                googleMap.addMarker(markerOptions)
-            }
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    private fun checkLocationServicesEnabled() {
+        val locationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        val isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+        if (!isGpsEnabled && !isNetworkEnabled) {
+            AlertDialog.Builder(requireContext())
+                .setMessage("Location Services are disabled. Would you like to enable them?")
+                .setPositiveButton("Yes") { _, _ ->
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(intent)
+                }
+                .setNegativeButton("No", null)
+                .show()
         }
-        return view
     }
 }
