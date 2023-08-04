784dc2c112bbed93fd5fec705e98e2258260c59c
Vladimir <alfa.voland@mail.ru>
2023-08-04 17:53:23 +0600

    FAT-58350: added user current location

--- app/src/main/java/com/example/pavliukovandersen/T5GoogleMapsFragment.kt
+++ app/src/main/java/com/example/pavliukovandersen/T5GoogleMapsFragment.kt
@@ -1,15 +1,21 @@
 package com.example.pavliukovandersen
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,14 +26,53 @@ import com.google.android.gms.maps.model.MarkerOptions
 class T5GoogleMapsFragment : Fragment() {
 
     private lateinit var mapView: MapView
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     private val mapReadyCallback = OnMapReadyCallback { googleMap ->
 
         checkLocationServicesEnabled()
 
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            try {
+                googleMap.isMyLocationEnabled = true
+
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location ->
+                        location?.let {
+                            val currentLocation = LatLng(it.latitude, it.longitude)
+                            googleMap.addMarker(
+                                MarkerOptions().position(currentLocation).title("You are here")
+                            )
+                            googleMap.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(currentLocation, 15f)
+                            )
+                        }
+                    }
+            } catch (e: SecurityException) {
+                Log.d("ERROR", "$e")
+            }
+        }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -60,6 +105,20 @@ class T5GoogleMapsFragment : Fragment() {
         mapView.onLowMemory()
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission was granted, reload the map or take other actions
+            } else {
+                // Permission was denied
+            }
+        }
+    }
+
     private fun checkLocationServicesEnabled() {
         val locationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
@@ -75,5 +134,9 @@ class T5GoogleMapsFragment : Fragment() {
                 .setNegativeButton("No", null)
                 .show()
         }
+    }
+
+    companion object {
+        const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
