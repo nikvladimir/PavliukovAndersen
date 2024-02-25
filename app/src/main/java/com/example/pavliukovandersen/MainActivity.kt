d49048fd17fd4bf04cd4b56005d44c02ed9318da
Vladimir <alfa.voland@mail.ru>
2024-02-25 08:30:01 +0600

    fixes: set current date for NewsListFragment

--- app/src/main/java/com/example/pavliukovandersen/MainActivity.kt
+++ app/src/main/java/com/example/pavliukovandersen/MainActivity.kt
@@ -45,9 +45,6 @@ class MainActivity : AppCompatActivity() {
                         R.id.displayed_screen_fl, T3NewsListFragment()
                     ).commit()
 
-//                    R.id.T9_DiskInfo -> fragmentManager.beginTransaction().replace(
-//                        R.id.displayed_screen_fl, T9DiskInfoFragment()
-//                    ).commit()
                 }
                 activityMainDrawer.closeDrawer(GravityCompat.START)
                 true
